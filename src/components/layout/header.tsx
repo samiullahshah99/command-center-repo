6f6702b669cbc623c5277f5fb60883388694e22d
samiullah6799 <samiullah6799@gmail.com>
2026-07-28 17:54:37 +0500

    fix: remove CtaGithub and leftover demo nav links

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,7 +5,6 @@ import { Breadcrumbs } from '../breadcrumbs';
 import SearchInput from '../search-input';
 import { ThemeSelector } from '../themes/theme-selector';
 import { ThemeModeToggle } from '../themes/theme-mode-toggle';
-import CtaGithub from './cta-github';
 
 export default function Header() {
   return (
@@ -17,7 +16,6 @@ export default function Header() {
       </div>
 
       <div className='flex items-center gap-2 px-4'>
-        <CtaGithub />
         <div className='hidden md:flex'>
           <SearchInput />
         </div>
